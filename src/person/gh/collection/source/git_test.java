8506ae38e1b0c2fdcc59f4c81099e272b1431166
GaoHang <catblack2021@163.com>
2022-02-14 17:03:22 +0800

    second commit

--- src/person/gh/collection/source/git_test.java
+++ src/person/gh/collection/source/git_test.java
@@ -3,5 +3,6 @@ package person.gh.collection.source;
 public class git_test {
     public static void main(String[] args) {
         System.out.println("hello,git");
+        System.out.println("second commit");
     }
 }
